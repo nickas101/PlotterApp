1892f59092a7150c7e89efe8c8ebf4c16aad7745
nickas101 <nickas101@gmail.com>
2019-01-20 19:51:31 +1300

    Parser dll is added.

---
+++ Parser/Parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public class Parser
+    {
+        public static string Parse(string[] lines)
+        {
+            Plotter.Plot(lines);
+
+            return "Hi from parser";
+        }
+    }
+}
---
+++ Perser/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perser
+{
+    public class Class1
+    {
+    }
+}
