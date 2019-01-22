bd7eabd6295ad7e96dd4e329ee66a8987e7b5cff
nickas101 <nickas101@gmail.com>
2019-01-22 19:13:54 +1300

    Methot Parse returns object Plot.

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -8,15 +8,11 @@ namespace Parser
 {
     public class Parser
     {
-        public static string Parse(string[] lines)
+        public static Plot Parse(string[] lines)
         {
-            //Plotter.Plot(lines);
             Plot thePlot = new Plot(1,2);
 
-            Console.WriteLine(thePlot.X);
-            Console.ReadKey();
-
-            return "Hi from parser";
+            return thePlot;
         }
     }
 }
