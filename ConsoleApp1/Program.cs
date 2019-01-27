ca9f334c272ecfd829d980f9c363b8aa002ce79b
nickas101 <nickas101@gmail.com>
2019-01-27 22:28:41 +1300

    Main Program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,13 +14,7 @@ namespace ConsoleApp1
 
             Parser.Plot plot = Parser.Parser.Parse(allLines);
 
-            //Console.Write(plot.X[1, 2] + "\n");
-            //Console.ReadKey();
-
             Plotter.DrowPlot(plot);
-
-            //Console.WriteLine(plot.Y);
-            //Console.ReadKey();
         }
     }
 }
